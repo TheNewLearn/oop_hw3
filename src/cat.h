999b262b35ae1a6229dfa721ac97fa57a38c3e11
unknown <sac05102634@gmail.com>
2021-11-12 23:03:54 +0800

    hw3

--- src/cat.h
+++ src/cat.h
@@ -1,24 +1,74 @@
+
+#ifndef CAT_H
+#define CAT_H
 #include <string>
 
 class Cat {
 public:
+  
+  
+  Cat(std::string name, double weight) {
+    _name = name;
+    _weight = weight;
+  }
+  
+  ~Cat() {
+    //delete [] _toyCollections;
+  }
 
-  Cat(std::string name, double weight) {}
+  std::string getName() {
+    return _name;
+  }
 
-  ~Cat() {}
+  double getWeight() {
+    return _weight;
+  }
+  
 
-  std::string getName() {}
+  void addNewToy(std::string toyName) {
 
-  double getWeight() {}
+    if(_toyAmount == 0){
+      _toyCollections = new std::string[_toyAmount+1];
+      _toyCollections[_toyAmount] =  toyName;
+      _toyAmount+=1;
+    }else{
 
-  void addNewToy(std::string toyName) {}
+    std::string* array;
+    array = _toyCollections;
+    _toyCollections = new std::string[_toyAmount+1];
+    for(int i=0;i<_toyAmount;i++){
+      _toyCollections[i] = array[i];
+    }
+    _toyCollections[_toyAmount] =  toyName;
+    _toyAmount+=1;
+    delete[] array;
+    
+    }
+    
+    
+    
+    
+    
+    
+  }
 
-  std::string getToy(int index) {}
+  std::string getToy(int index) {
+    if(index<0 || sizeof(_toyCollections) ==0 || index>_toyAmount - 1){
+      throw std::string("undefined");
+    }
+    else{
+      return _toyCollections[index];
+    }
+     
+  
+  }
+  
 
 private:
   std::string _name;
   double _weight;
   std::string * _toyCollections;
-  int _toyAmount;
+  int _toyAmount =0;
 
 };
+#endif
--- test/ut_cat.h
+++ test/ut_cat.h
@@ -1,5 +1,27 @@
 #include "../src/cat.h"
-
+#include<string>
 #include <gtest/gtest.h>
 
 // add your test here
+TEST(CATTEST,cat){
+Cat w ("winny",12.5);
+ASSERT_EQ(12.5,w.getWeight());
+ASSERT_EQ("winny",w.getName());
+ASSERT_NEAR(12.5,w.getWeight(),0.0001);
+
+}
+
+TEST(CATEST2,add_toy){
+    Cat b("denny",5);
+    b.addNewToy("car");
+    b.addNewToy("gun");
+    b.addNewToy("gun2");
+
+    
+
+    ASSERT_EQ(b.getToy(2),"gun2");
+    ASSERT_EQ(b.getToy(0),"car");
+    ASSERT_EQ(b.getToy(1),"gun");
+    ASSERT_THROW(b.getToy(-1),std::string);
+    ASSERT_THROW(b.getToy(3),std::string);
+}
